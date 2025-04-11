fc79066fff8cc596599962c9e77bf712a4ca8bc2
SriGanesh-73 <sriganeshthiyagarajan@gmail.com>
2025-04-11 18:45:04 +0530

    Update in app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify,render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
@@ -58,7 +58,8 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port)
 
 
         
